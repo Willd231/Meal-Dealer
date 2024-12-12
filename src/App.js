5aeea56c69c22e070510ba06a63a53f9e69882b0
Will Dellinger <dellinger.w3@gmail.com>
2024-12-12 13:24:20 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+
+
+function Header(){
+  return(
+    <div class = "H-Container">
+      <div class = "H-1"></div>
+      <div class = "H-2"></div>
+      <div class = "H-3"></div>
+    </div>
+  );
+}
+
+
+
+
+
+function App() {
+  return (
+    <h1>Hi</h1>
+  );
+}
+
+export default App;
